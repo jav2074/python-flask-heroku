d204ada5c16bd905e37371b530b2992140c81a02
jav2074 <jav.electrochance@gmail.com>
2020-04-30 01:02:57 -0300

    Add database.db

--- index.py
+++ index.py
@@ -1,5 +1,33 @@
 from flask import Flask, render_template
 
+# -------------------------------------------------------------------------------
+# DB
+# -------------------------------------------------------------------------------
+import sqlite3
+
+# connection dir property
+db_name = 'database.db'      # '../src/database.db'
+
+# Function to Execute Database Querys
+def run_query(query, parameters = ()):
+    with sqlite3.connect(db_name) as conn:
+        cursor = conn.cursor()
+        result = cursor.execute(query, parameters)
+        conn.commit()
+    return result
+
+# Get Products from Database
+def get_products():
+    # getting data
+    query = 'SELECT * FROM product ORDER BY name DESC'
+    db_rows = run_query(query)
+    # filling data
+    for row in db_rows:
+        print(row)
+
+# -------------------------------------------------------------------------------
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,7 +40,9 @@ def about():
 
 @app.route('/db')
 def db():
+    get_products()
     return render_template('db.html')
 
 if __name__ == '__main__':
+    # application = Product(app)
     app.run(debug=True)     # Modo debug 
