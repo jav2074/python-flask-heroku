172ee411f1d98206f3c12657a981ed14fbf71b6e
jav2074 <jav.electrochance@gmail.com>
2020-04-30 18:36:25 -0300

    Add h_database

--- index.py
+++ index.py
@@ -8,14 +8,6 @@ import sqlite3
 # connection dir property
 db_name = 'h_database.db'             # HEROKU
 
-def db_init(host):    
-    if(host == '127.0.0.1'):
-        db = 'src/l_database.db'     # LOCAL
-    else:
-        db = 'h_database.db'         # HEROKU
-    print(f'host: {host} - db: {db}')
-    return db
-
 # Function to Execute Database Querys
 def run_query(query, parameters = ()):
     with sqlite3.connect(db_name) as conn:
@@ -44,7 +36,6 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'    # para flash
 
 @app.route('/')
 def home():
-    db_name = db_init(request.remote_addr)
     return render_template('home.html')
 
 @app.route("/get_my_ip", methods=["GET"])
