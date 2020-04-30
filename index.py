0e65d8718c1c4d962e2eee7d412266a23174308e
jav2074 <jav.electrochance@gmail.com>
2020-04-30 17:36:53 -0300

    Add db_init(host)

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
 # -------------------------------------------------------------------------------
 # DB
@@ -6,15 +6,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
 
 # connection dir property
-db_name = 'database.db'
-# db_name = 'src/database.db'       # LOCAL
+db_name = 'database.db'             # HEROKU
+
+def db_init(host):    
+    if(str(host) == '127.0.0.1'):
+        db_name = 'src/database.db'     # LOCAL
+    else:
+        db_name = 'database.db'         # HEROKU
+    print(f'host: {host} - db: {db_name}')
 
 # Function to Execute Database Querys
 def run_query(query, parameters = ()):
     with sqlite3.connect(db_name) as conn:
         cursor = conn.cursor()
-        result = cursor.execute(query, parameters)   # result = 
-        # result = cursor.fetchall()          # convierte el resultset en un Array
+        result = cursor.execute(query, parameters) 
         conn.commit()
     return result
 
@@ -37,6 +42,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'    # para flash
 
 @app.route('/')
 def home():
+    db_init(request.remote_addr)
     return render_template('home.html')
 
 @app.route('/about')
@@ -52,8 +58,11 @@ def db():
 def db_delete(id):
     result = delete_products(id)
     flash(f"Se ha borrado correctamente el registro id: {id}")
-    # flash("Se ha borrado correctamente el registro id: ")
     return redirect(url_for('db'))
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)     # Modo debug 
