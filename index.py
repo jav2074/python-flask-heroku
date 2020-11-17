ee2616ad6810a503296d0dcd9834009ea022849e
jav2074 <jav.electrochance@gmail.com>
2020-11-17 13:50:52 -0300

    Refactor

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from __init__ import create_app
 from flask import render_template, request, redirect, url_for, flash, jsonify
+from __init__ import create_app
 from bd import *
 
 # -------------------------------------------------------------------------------
